048985c583e4a93583c90e3581a72b05cdb9bad1
john-sousa96 <69728894+john-sousa96@users.noreply.github.com>
2023-02-26 15:18:59 -0300

    arquivos iniciais

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <!-- Meta tags Obrigatórias -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="estilo.css">
+  <title>Mercadin - Página Inicial</title>
+</head>
+
+<header>
+ 
+    <nav class="navbar navbar-expand-sm navbar-light" style="background-color: #ffffff;">
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav">
+          <li class="nav-item"> <a class="nav-link" href="index.php"><img src="img/orange.png" width="30"
+                height="30"> | </a></li>
+          <li class="nav-item"> <a class="nav-link" href="#"> <img src="img/localizacao.png" width="25" height="25">
+              Enviar para o CEP</a></li>
+          <!-- ícone acima criado por <a href="https://www.flaticon.com/br/icones-gratis/gps" title="gps ícones">Gps ícones criados por Phoenix Group - Flaticon</a> -->
+
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+          </form>
+          <li class="nav-item"> <a class="nav-link" href="#"><img src="img/login.png" width="30"
+            height="30"></a></li>
+      </div>
+    </nav>
+
+    <nav class="navbar  navbar-expand-sm navbar-light"  style="background-color: #ffffff;">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav">
+          <li class="nav-item"> <a class="nav-link" href="#">Básico da Dispensa</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#">Açougue e Peixaria</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#">HortiFruti</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#">Laticínios</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#">Padaria</a></li>
+        </ul>
+      </div>
+    </nav>
+ 
+</header>
+
+<body>
+</body>
+
+<footer>
+  <p>Mercadin é uma aplicação desenvolvida por alunos do curso de Análise e Desenvolvimento de Sistemas do Senac - Todos
+    os direitos reservados</p>
+</footer>
+
+</html>
